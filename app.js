560d88c18910c8a531cac0a5c3ace8b7807c39e7
Aneteron <99216347+Aneteron@users.noreply.github.com>
2023-01-05 23:03:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
 
-		this.camera = THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
+		this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
 		this.camera.position.set(0,0,4);
 
 		this.scene = new THREE.Scene();
@@ -15,7 +15,7 @@ class App{
 		this.renderer = new THREE.WebGLRenderer( { antialising: true});
 		this.renderer.setPixelRatio(window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
-		container.appendChild( this.render.domElement );
+		container.appendChild( this.renderer.domElement );
 
 		this.renderer.setAnimationLoop ( this.render.bind(this) );
 
